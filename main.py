4c005025c7ca2d116922bca06c768b16f197af7b
david <yuchild@gmail.com>
2020-05-09 20:37:43 -0700

    Updated Simple Chatbot

---
+++ main.py
@@ -0,0 +1,13 @@
+import nltk
+import nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+
+with open('intents.json') as file:
+    data = json.load(file)
+print(data)
